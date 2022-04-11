16c681297feef1769f5fbdac4b3718d54eb44ee9
jaemin <fkdlem524@naver.com>
2022-04-11 13:50:11 +0900

    커밋

--- src/main/java/com/sesac/foodtruckorder/infrastructure/query/http/dto/GetItemsInfoDto.java
+++ src/main/java/com/sesac/foodtruckorder/infrastructure/query/http/dto/GetItemsInfoDto.java
@@ -1,7 +1,14 @@
 package com.sesac.foodtruckorder.infrastructure.query.http.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+//@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class GetItemsInfoDto {
     private Long itemId;        // 아이템 ID
